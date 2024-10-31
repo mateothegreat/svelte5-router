b820a35f36ae1786a3a680f5d1e75bab4f180497
Matthew Davis <matthew@matthewdavis.io>
2024-10-31 16:34:55 -0500

    get query param

--- src/lib/methods.ts
+++ src/lib/methods.ts
@@ -1,3 +1,7 @@
 export const goto = (path: string) => {
   window.history.pushState({}, "", path);
 };
+
+export const query = (key: string): string | null => {
+  return new URLSearchParams(window.location.search).get(key);
+};
