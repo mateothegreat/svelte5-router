2230c04c60b10592df2da9fd284906992bd3087f
Matthew Davis <matthew@matthewdavis.io>
2024-10-31 16:52:35 -0500

    bump

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { route } from "./actions/route";
 export * from "./instance.svelte";
-export { goto } from "./methods";
+export { goto, query } from "./methods";
+export { QueryString } from "./query.svelte";
 export * from "./router.svelte";
 export { default as Router } from "./router.svelte";
 
