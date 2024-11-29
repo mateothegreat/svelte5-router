19d910f26f5cb1932a383ff08a023d1cd29a5de3
Matthew Davis <matthew@matthewdavis.io>
2024-11-29 12:15:26 -0600

    bump

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,9 @@
 export { route } from "./actions/route";
-
-export { Instance };
-import { Instance } from "./instance.svelte";
-
-import { QueryString } from "./query.svelte";
-export { QueryString };
-
-import Router from "./router.svelte";
-export { Router };
-
 export { goto, query } from "./methods";
+export * from "./router.svelte";
 
+import { Instance } from "./instance.svelte";
+import { QueryString } from "./query.svelte";
+import Router from "./router.svelte";
+
+export { Instance as default, QueryString, Router };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { defineConfig } from "vite";
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [svelte()]
+	plugins: [tsconfigPaths(), svelte({
+		preprocess: [sveltePreprocess({ typescript: true })]
+	})
+	],
 });
