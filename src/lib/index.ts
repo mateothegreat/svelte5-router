c1a12a58e2153accf5ef3f9a2757f4ae8c7e267e
Matthew Davis <matthew@matthewdavis.io>
2024-11-29 11:55:07 -0600

    fix index.ts exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,13 @@
 export { route } from "./actions/route";
+
 export { Instance };
 import { Instance } from "./instance.svelte";
-import { QueryString } from "./query.svelte";
-import Router from "./router.svelte";
 
-export { goto, query } from "./methods";
+import { QueryString } from "./query.svelte";
 export { QueryString };
 
+import Router from "./router.svelte";
 export { Router };
+
+export { goto, query } from "./methods";
+
