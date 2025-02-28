9d4e82841eb0a5abaa3867b2852ed72af15c13b9
Matthew Davis <matthew@matthewdavis.io>
2025-02-27 20:09:13 -0600

    fix: package.json exports causing errors

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,12 +9,8 @@ export { Query } from "./helpers/query.svelte";
 export type { Hooks } from "./hooks";
 export { log as logger } from "./logger";
 export type { RouteParams as Params } from "./params";
+export { registry, type Registry } from "./registry.svelte";
 export type { Route, Routed } from "./route.svelte";
 export { RouterInstanceConfig } from "./router-instance-config";
-export { Registry, RouterInstance };
-import { Registry } from "./registry.svelte";
-import * as RouterInstance from "./router-instance.svelte";
-
+export { RouterInstance } from "./router-instance.svelte";
 export { default as Router } from "./router.svelte";
-
-export { registry } from "./registry";
